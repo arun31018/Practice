892c0be580cac560a6b465cd1048a9b9e63a3dce
arunkumar31018@gmail.com <arunkumar31018@gmail.com>
2020-11-28 19:58:38 +0530

    login controller changes

--- src/main/java/com/springpractice/controller/LoginController.java
+++ src/main/java/com/springpractice/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.springpractice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LoginController {
+	
+@RequestMapping(value="/welcometogit",method = RequestMethod.GET)	
 public void m1() {
 	System.out.println("git practice");
 }
