13ff5db39410cb5d503b34055cfbe5bbdf3b09c9
arun31018 <arunkumar31018@gmail.com>
2020-12-20 13:51:13 +0530

    commit in master branch

--- src/main/java/com/springpractice/controller/LoginController.java
+++ src/main/java/com/springpractice/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.springpractice.controller;
 
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,6 @@ public class LoginController {
 public void m1() {
 
 	System.out.println("Hello World !!");
+	System.out.println("welcome to git hub");
 }
 }
